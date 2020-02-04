117942d3b764c70c8e643b7db60f59ec7f43b01a
Joshua D'Andrea <jdandrea@uccs.edu>
2020-02-04 11:25:40 -0700

    Lose Con Finished

--- GDD_CAPSTONE/Assets/Scripts/Utilities_Managers/HUDManager.cs
+++ GDD_CAPSTONE/Assets/Scripts/Utilities_Managers/HUDManager.cs
@@ -12,6 +12,7 @@ public class HUDManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameplayManager.EnemiesKilled = 0;
         enemiesKilledText.text = " Enemies Killed: " + GameplayManager.EnemiesKilled;
         baseHealthText.text = " Base Health: " + GameplayManager.BaseHealth;
     }
