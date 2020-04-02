03ea8c689d7958728832483500a7a665977dea23
Joshua D'Andrea <jdandrea@uccs.edu>
2020-04-02 17:00:14 -0600

    Basis for BA placement

---
+++ GDD_CAPSTONE/Assets/Scripts/A Test Folder/TileMapTester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapTester : MonoBehaviour
+{
+    [SerializeField] GameObject build;
+    Tilemap tilemp;
+    void Start() 
+    { 
+        tilemp = GameObject.Find("Grass").GetComponent<Tilemap>();
+        Debug.Log(tilemp.ToString());
+    }
+    void Update() 
+    { 
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3Int selectedTile = tilemp.WorldToCell(point);
+            Debug.Log(tilemp.GetTile(selectedTile));
+            if (tilemp.ContainsTile(tilemp.GetTile(selectedTile)))
+            {
+                Instantiate(build, new Vector2(selectedTile.x + 1, selectedTile.y + 1), Quaternion.identity);
+            }
+
+
+        }
+    }
+
+    //private void OnMouseEnter()
+    //{
+    //    gameObject.SetActive(false);
+    //}
+}
