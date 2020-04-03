eb234daa595e97446d3087a6e0d803af10e92994
Joshua D'Andrea <jdandrea@uccs.edu>
2020-04-02 22:01:10 -0600

    It "works"

--- GDD_CAPSTONE/Assets/Scripts/A Test Folder/TileMapTester.cs
+++ GDD_CAPSTONE/Assets/Scripts/A Test Folder/TileMapTester.cs
@@ -6,26 +6,50 @@ using UnityEngine.Tilemaps;
 public class TileMapTester : MonoBehaviour
 {
     [SerializeField] GameObject build;
-    Tilemap tilemp;
+    [SerializeField] Tile hoverTile;
+    [SerializeField] Tile builtTile;
+    [SerializeField] Tilemap tilemp;
+    Vector3Int selectedTile;
+    Color white;
+    Color black;
     void Start() 
     { 
-        tilemp = GameObject.Find("Grass").GetComponent<Tilemap>();
-        Debug.Log(tilemp.ToString());
+        //tilemp = GameObject.Find("EnemyPath").GetComponent<Tilemap>();
+
+        white = new Color(1, 1, 1, 1);
+        black = new Color(100, 100, 100, 1);
     }
     void Update() 
     { 
         Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3Int selectedTile = tilemp.WorldToCell(point);
-            Debug.Log(tilemp.GetTile(selectedTile));
-            if (tilemp.ContainsTile(tilemp.GetTile(selectedTile)))
+            selectedTile = tilemp.WorldToCell(point);
+            
+            if (tilemp.ContainsTile(tilemp.GetTile(selectedTile)) && tilemp.GetTile(selectedTile) != builtTile)
             {
-                Instantiate(build, new Vector2(selectedTile.x + 1, selectedTile.y + 1), Quaternion.identity);
+                tilemp.SetTile(selectedTile, builtTile);
+                Instantiate(build, new Vector2(selectedTile.x + .5f, selectedTile.y + .5f), Quaternion.identity);
+                Debug.Log("Poop");
             }
 
 
         }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (tilemp.ContainsTile(tilemp.GetTile(selectedTile)) && tilemp.GetTile(selectedTile) != builtTile)
+        {
+            tilemp.SetTile(selectedTile,hoverTile);
+            Debug.Log("HOVER");
+        }
+    }
+    private void OnMouseExit()
+    {
+        tilemp.SetTile(selectedTile, builtTile);
     }
 
     //private void OnMouseEnter()
